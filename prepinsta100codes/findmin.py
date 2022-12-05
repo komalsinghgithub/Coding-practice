af477bfd5457689ae1ded6b40d8709212de26384
komal singh <singhkomal8823@gmail.com>
2022-12-05 19:03:43 +0530

    5/12/22

---
+++ prepinsta100codes/findmin.py
@@ -0,0 +1,10 @@
+# Find the maximum from the array given and return it...
+
+def findmax(array):
+    # write your code here
+    minimum=min(array)
+    return minimum 
+
+
+array=[2,7,98,1,-6,56,6]
+print(findmax(array))
---
+++ prepinsta100codes/findminmax.py
@@ -0,0 +1,11 @@
+# Find the maximum from the array given and return it...
+
+def findmax(array):
+    # write your code here
+    minimum=min(array)
+    maximum=max(array)
+    return minimum,maximum
+
+
+array=[2,7,98,1,-6,56,6]
+print(findmax(array))
