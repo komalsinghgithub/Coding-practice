695a6ee47acd252799944cdbab33bdb7dda64fcf
komal singh <singhkomal8823@gmail.com>
2023-01-04 19:21:31 +0530

    1/4/23

---
+++ codechef/Squats.py
@@ -0,0 +1,10 @@
+# Somu went to the gym today. He decided to do XX sets of squats. Each set consists of 1515 squats. Determine the total number of squats that he did today.
+
+tc= int(input())
+while(tc):
+    x=int(input())
+    y=x*15
+    print(y)
+    tc=tc-1
+
+
